ecdc410e06e693c3041603546f43e12f099c56d8
chengyao <bigyao@139.com>
2020-12-30 21:02:35 +0800

    add routes alias

---
+++ src/Route/Alias.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Yao\Route;
+
+class Alias
+{
+    protected array $alias = [];
+
+    public function set($alias, string $path)
+    {
+        if ($alias) {
+            if (isset($this->alias[$alias])) {
+                throw new \Exception("Path:'{$path}'的别名'{$alias}'已经被注册！");
+            }
+            $this->alias[$alias] = $path;
+        }
+    }
+
+    public function get(?string $alias = null)
+    {
+        if (is_null($alias)) {
+            return $this->alias;
+        }
+        return $this->alias[$alias] ?? null;
+    }
+}
