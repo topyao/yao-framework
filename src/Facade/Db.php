27f2e9967bfed96449019ed59b3f134c3689c851
chengyao <bigyao@139.com>
2021-01-06 23:52:34 +0800

    添加可静态调用类的限制

--- src/Facade/Db.php
+++ src/Facade/Db.php
@@ -19,4 +19,14 @@ class Db extends Facade
         return \Yao\Db::class;
     }
 
+    public static function __callStatic($method, $params)
+    {
+        $callAble = ['name', 'query', 'exec'];
+        if (in_array($method, $callAble)) {
+            return call_user_func_array([static::createFacade(), $method], $params);
+        } else {
+            throw new \Exception("方法{$method}不允许被静态调用!");
+        }
+    }
+
 }
