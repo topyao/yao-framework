775a738ad4fc4846dfaf9b55596e997a94e43e4c
chengyao <bigyao@139.com>
2021-01-06 23:46:42 +0800

    db的门面优化

--- src/Facade/Db.php
+++ src/Facade/Db.php
@@ -3,13 +3,14 @@
 
 namespace Yao\Facade;
 
-
 use Yao\Facade;
 
 /**
  * Class Db
  * @package Yao\Facade
  * @method static \Yao\Db name(string $table_name)
+ * @method static mixed query(string $sql, ?array $data = [], bool $all = true)
+ * @method static int exec(string $sql, array $data = [])
  */
 class Db extends Facade
 {
