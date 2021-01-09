f534e7d7ea145408ccacd535ab2b1bb1705b4a83
chengyao <bigyao@139.com>
2021-01-09 15:43:37 +0800

    添加了facade的方法注释

--- src/Facade/Json.php
+++ src/Facade/Json.php
@@ -2,6 +2,11 @@
 
 namespace Yao\Facade;
 
+/**
+ * Class Json
+ * @package Yao\Facade
+ * @method static \Yao\Http\Response\Json data($data)
+ */
 class Json extends \Yao\Facade
 {
     protected static function getFacadeClass()
--- src/Facade/Response.php
+++ src/Facade/Response.php
@@ -3,6 +3,7 @@
 namespace Yao\Facade;
 
 /**
+ * @method static \Yao\Http\Response data($data)
  * @method static get(string $key = null, $default = null)
  * Class Config
  * @package Yao\Facade
