6858870c36478cc81ba309657d3d6a642940f6e3
chengyao <bigyao@139.com>
2021-01-09 15:42:43 +0800

    添加了路由别名参数数量验证

--- src/Route/Alias.php
+++ src/Route/Alias.php
@@ -25,6 +25,9 @@ class Alias
         if (isset($this->alias[$alias])) {
             if (preg_match('/\(.+\)/i', $this->alias[$alias])) {
                 $rep = explode(',', preg_replace(['#\\\#', '#\(.+\)#Ui'], ['', ','], $this->alias[$alias]));
+                if (($argNums = count($rep) - 1) != count($args)) {
+                    throw new \Exception("别名:{$alias}需要传入{$argNums}个参数！");
+                }
                 $match = '';
                 foreach ($rep as $k => $r) {
                     $match .= ($r . ($args[$k] ?? ''));
