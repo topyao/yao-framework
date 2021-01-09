eb28c33a3b5228c5b623b9c2aa8d91fdc27d6d0f
chengyao <bigyao@139.com>
2021-01-09 23:14:36 +0800

    添加路由缓存功能

--- src/Route.php
+++ src/Route.php
@@ -210,9 +210,13 @@ class Route
 
     public function register()
     {
-        array_map(
-            fn($routes) => require_once($routes),
-            glob(ROOT . 'route' . DIRECTORY_SEPARATOR . '*' . 'php')
-        );
+        if (file_exists($routes = ROOT . 'bootstrap' . DIRECTORY_SEPARATOR . 'cache' . DIRECTORY_SEPARATOR . 'app' . DIRECTORY_SEPARATOR . 'routes.php')) {
+            $this->routes = unserialize(file_get_contents($routes));
+        } else {
+            array_map(
+                fn($routes) => require_once($routes),
+                glob(ROOT . 'route' . DIRECTORY_SEPARATOR . '*' . 'php')
+            );
+        }
     }
 }
