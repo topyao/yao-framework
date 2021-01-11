b4b9868f9c63433f9cddd1cb89738ee45b3210bc
chengyao <bigyao@139.com>
2021-01-11 20:44:31 +0800

    添加了方法注解

--- src/Facade/Db.php
+++ src/Facade/Db.php
@@ -9,9 +9,9 @@ use Yao\Facade;
 /**
  * Class Db
  * @package Yao\Facade
- * @method static \Yao\Db name(string $table_name)
- * @method static mixed query(string $sql, ?array $data = [], bool $all = true)
- * @method static int exec(string $sql, array $data = [])
+ * @method static \Yao\Db\Driver name(string $table_name) 表名设置方法
+ * @method static \Yao\Db\Driver query(string $sql, ?array $data = [], bool $all = true)
+ * @method static \Yao\Db\Driver exec(string $sql, array $data = [])
  */
 class Db extends Facade
 {
