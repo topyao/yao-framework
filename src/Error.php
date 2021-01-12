f97a69c7b1d3ae4944bef738ebf95a67a70b7f91
chengyao <chengyao1@foxmail.com>
2021-01-12 17:21:10 +0800

    update

--- src/Error.php
+++ src/Error.php
@@ -46,7 +46,7 @@ class Error
         Log::write('system', $message, 'notice', ['请求地址:' . $_SERVER['REQUEST_URI'], 'trace' . $exception->getTraceAsString()]);
         http_response_code((int)$exception->getCode());
         if ($this->debug) {
-            exit(' <title>' . $message . ' </title><style>.box{width:800px;height:4em;margin:2em auto}</style><pre class="box"><div class="title"><b>Message:</b>' . $message . ' </div><div class="box2" style = "word-wrap:break-word;word-break: break-all;color:dimgrey" ><br><b>Code:</b>' . $code . ' <br><b>Locate:</b>' . $exception->getFile() . '&nbsp; line:' . $exception->getLine() . ' <p>Trace:</p>' . $exception->getTraceAsString() . ' </div> </pre>');
+            exit(' <title>' . $message . ' </title><style>.box{width:800px;height:4em;margin:2em auto}</style><pre class="box"><div class="title">' . $message . ' </div><div class="box2" style = "word-wrap:break-word;word-break: break-all;color:dimgrey" ><b>Code:</b>' . $code . ' <br><b>Locate:</b>' . $exception->getFile() . '&nbsp; line:' . $exception->getLine() . ' <p>Trace:</p>' . $exception->getTraceAsString() . ' </div> </pre>');
         }
         exit(include_once $this->exceptionView);
     }
