6d336cc654e5d352b7e78494396afd6bec567477
chengyao <bigyao@139.com>
2021-01-28 19:54:55 +0800

    修改了注释

--- src/Facade/Db.php
+++ src/Facade/Db.php
@@ -10,8 +10,8 @@ use Yao\Facade;
  * Class Db
  * @package Yao\Facade
  * @method static \Yao\Db\Driver name(string $table_name) 表名设置方法
- * @method static \Yao\Db\Driver query(string $sql, ?array $data = [], bool $all = true)
- * @method static \Yao\Db\Driver exec(string $sql, array $data = [])
+ * @method static array|false query(string $sql, ?array $data = [], bool $all = true)
+ * @method static integer exec(string $sql, array $data = [])
  */
 class Db extends Facade
 {
