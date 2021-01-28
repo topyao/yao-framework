d1bea173de35306641545223ddcbe94bdd4a2c7e
chengyao <64066545+topyao@users.noreply.github.com>
2021-01-28 12:24:41 +0800

    添加了模型的一些方法【未测试】

--- src/Model.php
+++ src/Model.php
@@ -4,5 +4,14 @@ namespace Yao;
 
 class Model
 {
+   
+    public function __construct()
+    {
+        $this->name = rtrim(strchr(get_called_class(),'\\'),'\\');
+    }
 
+
+    public function __call($function_name,$arguments){
+        return Db::name($this->name)->$function_name(...$arguments);
+    }
 }
