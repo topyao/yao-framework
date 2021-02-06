780faeae3471363dd974f903d350ed1412782f60
chengyao <bigyao@139.com>
2021-02-06 12:14:06 +0800

    优化了error类

--- src/Error.php
+++ src/Error.php
@@ -19,17 +19,17 @@ class Error
     protected string $exceptionView;
 
 
-    public function __construct(\Yao\Config $config)
+    public function __construct(App $app)
     {
-        $this->config = $config;
-        $this->debug = $this->config->get('app.debug');
+        $this->app = $app;
+        $this->debug = $this->app['config']->get('app.debug');
         $iniSet = [
             [true => 'On', false => 'Off'],
             [true => E_ALL, false => 0]
         ];
         function_exists('ini_set') && ini_set('display_errors', $iniSet[0][$this->debug]);
         error_reporting($iniSet[1][$this->debug]);
-        $this->exceptionView = $this->config->get('app.exception_view') ?: dirname(__FILE__) . DIRECTORY_SEPARATOR . 'Tpl' . DIRECTORY_SEPARATOR . 'exception.html';
+        $this->exceptionView = $this->app['config']->get('app.exception_view') ?: dirname(__FILE__) . DIRECTORY_SEPARATOR . 'Tpl' . DIRECTORY_SEPARATOR . 'exception.html';
     }
 
     public function register()
@@ -43,7 +43,7 @@ class Error
     {
         $code = $exception->getCode() ?: 'Exception';
         $message = $exception->getMessage();
-        Log::write('system', $message, 'notice');
+        Log::write('system', $message, 'notice', ['Method' => $this->app['request']->method(), 'Path' => $this->app['request']->path()]);
         if ($this->debug) {
             $data = '<!DOCTYPE html>
             <html lang="zh">
@@ -60,18 +60,18 @@ class Error
         } else {
             $data = include_once $this->exceptionView;
         }
-        return Response::data($data)->code((int)$exception->getCode())->return();
+        exit($this->app['response']->data($data)->code((int)$exception->getCode())->return());
     }
 
     public function error($code, $message, $file, $line, $errContext)
     {
-        Log::write('system', $message, 'notice', [$code, $file, $line]);
+        Log::write('system', $message, 'notice', ['Method' => $this->app['request']->method(), 'Path' => $this->app['request']->path(), $code, $file, $line]);
         if ($this->debug) {
-            $data = ' <title>' . $message . '</title><pre style = "font-size:1.6em"><b> Message:</b> ' . $message . ' <br><b> Code:</b> ' . $code . ' <br><b> Location:</b> ' . $file . ' + ' . $line . '<br><b> Trace:</b>' . print_r($errContext, true) . '</pre> ';
+            $data = ' <title>' .$message . '</title><b>Message:</b> ' . $message . ' <br><b>Code:</b>' . $code . ' <br><b>Location:</b> ' . $file . ' + ' . $line . '<br><b>Trace:</b><br>' . print_r($errContext, true);
         } else {
             $data = include_once $this->exceptionView;
         }
-        return Response::data($data)->code(403)->return();
+        exit($this->app['response']->data($data)->code(403)->return());
     }
 
     public function shutdown()
