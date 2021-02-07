a45fb11dd1501bc63de90beba2d9306a965dc330
chengyao <bigyao@139.com>
2021-02-07 16:20:28 +0800

    添加了Db服务

---
+++ src/Db/DatabaseService.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace {
+
+    if (false === function_exists('db')) {
+        /**
+         * Db类助手函数
+         * @param string $tableName
+         * @return \Yao\Db
+         */
+        function db(string $tableName)
+        {
+            return \Yao\Facade\Db::name($tableName);
+        }
+    }
+
+}
+
+namespace Yao\Db {
+
+    use Yao\Provider\Service;
+
+    class DatabaseService implements Service
+    {
+        public function boot()
+        {
+
+        }
+
+        public function register()
+        {
+            // TODO: Implement register() method.
+        }
+
+    }
+}
+
