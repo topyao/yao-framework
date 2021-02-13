bc49b1172b6fddec6f8104275462696ee16f9a84
chengyao <bigyao@139.com>
2021-02-13 21:04:40 +0800

    添加了App的门面

--- src/Facade/App.php
+++ src/Facade/App.php
@@ -9,7 +9,7 @@ use Yao\Facade;
 /**
  * @method static get(string $class_name) 获取实例化后的对象
  * @method static has(string $class_name) 判断实例是否存在
- * @method void static bind(string $id, string $className) 绑定类到标识ID
+ * @method static bind(string $id, string $className) 绑定类到标识ID
  * @method static make(string $abstract, array $arguments = [], bool $singleInstance = true) 使用容器实例化类
  * @method static remove(string $abstract) 注销实例
  * @method static invokeMethod(array $callable, array $arguments = [], bool $singleInstance = true, array $constructorParameters = []) 对调用的方法实现依赖注入
