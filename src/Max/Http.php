8832a4cad1f17a093cb5cf9b599026f5f067409b
chengyao <bigyao@139.com>
2021-06-08 21:41:33 +0800

    dev -> create Http APP

---
+++ src/Max/Http.php
@@ -0,0 +1,45 @@
+<?php
+
+
+namespace Max;
+
+
+use Max\Foundation\App;
+
+class Http
+{
+
+    /**
+     * @var App
+     */
+    protected $app;
+
+    public function __construct(App $app)
+    {
+        $this->app = $app;
+    }
+
+    public function response()
+    {
+        ob_start();
+        $config     = $this->app['config']->get('app');
+        $this->bind = array_merge($config['alias'], $this->bind);
+        $this->app['error']->register();
+        $this->app['lang']->import($config['language']);
+        $this->app['provider']->serve($config['provider']['http'] ?? []);
+        date_default_timezone_set($config['default_timezone']);
+        return $this->app->middleware
+            ->through($config['middleware'])
+            ->then(function () {
+                return $this->app['route']->register()->dispatch();
+            })->end();
+    }
+
+    public function end($response)
+    {
+        return $this->app->response
+            ->body($response)
+            ->send();
+    }
+
+}
