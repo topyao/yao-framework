daf1a998fa39e0adb73acc46555df4fe96bf8297
chengyao <64066545+topyao@users.noreply.github.com>
2021-06-13 05:00:02 -0500

    dev -> Bug fixed.

--- src/Max/Http/Request.php
+++ src/Max/Http/Request.php
@@ -287,7 +287,7 @@ class Request implements RequestInterface
                 $ip = false;
             }
             for ($i = 0; $i < count($ips); $i++) {
-                if (!preg_match('^(10│172.16│192.168).', $ips[$i])) {
+                if (!preg_match('/^(10│172.16│192.168)./', $ips[$i])) {
                     $ip = $ips[$i];
                     break;
                 }
